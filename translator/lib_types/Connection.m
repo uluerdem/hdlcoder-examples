48e0ffc1a712e51fd2a928ee797166a406ff9e19
uluerdem <erdemulucinar@gmail.com>
2021-12-14 20:13:47 +0300

    Indicator for more than one wires connected to some pin is added.
    addConnection function returns an additional output indicating if the connected node's input pin is already connected to some other node to accommodate overridden input pin error check.

--- translator/lib_types/Connection.m
+++ translator/lib_types/Connection.m
@@ -12,11 +12,17 @@ classdef Connection < handle
             this.delays = cell(len,1);
         end
 
-        function this = addConnection(this,pin_idx,node,connected_pin,delay)
+        function [already_exist,this] = addConnection(this,pin_idx,node,connected_pin,delay)
             curr_len = length(this.connected_nodes{pin_idx});
             this.connected_nodes{pin_idx}(curr_len+1) = node;
             this.connected_pins{pin_idx}(curr_len+1) = connected_pin;
             this.delays{pin_idx}(curr_len+1) = delay;
+
+            if curr_len > 0
+                already_exist = 1;
+            else
+                already_exist = 0;
+            end
         end
     end
 end
